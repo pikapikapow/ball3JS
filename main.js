58df83f859faaffcab358172d8199dad9855346b
pikapikapow <vladimirhwang84@gmail.com>
2023-08-16 16:27:51 -0400

    3D ball done

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import "./style.css"
+import gsap from "gsap"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 //Scene
@@ -36,11 +37,18 @@ scene.add(camera)
 
 //Renderer
 const canvas = document.querySelector('.webgl')
-// console.log(canvas)
-
 const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(2)
 renderer.render(scene, camera)
+
+//controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
 
 //Resize
 window.addEventListener('resize', () => {
@@ -53,12 +61,40 @@ window.addEventListener('resize', () => {
   renderer.setSize(sizes.width, sizes.height)
 })
 
-//controls
-const controls = new OrbitControls(camera, canvas)
 
 
 const loop = () => {
+  controls.update()
   renderer.render(scene, camera)
   window.requestAnimationFrame(loop)
 }
 loop()
+
+//timeline magic
+const tl = gsap.timeline({ defaults: { duration: 1 } })
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 })
+tl.fromTo('nav', { y: '-100%' }, { y: "0%" })
+tl.fromTo('title', { opacity: 0 }, { opacity: 1 })
+
+//Mouse Animation Color
+let mouseDown = false
+let rgb = []
+window.addEventListener('mousedown', () => (mouseDown = true))
+window.addEventListener('mouseup', () => (mouseDown = false))
+
+window.addEventListener('mousemove', (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.width) * 255),
+      150
+    ]
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b
+    })
+  }
+})
